913e2e8d5827e57b875f47c3ff2f83de711cd3e1
wisdom ogbonna <doph574@gmail.com>
2023-04-04 20:14:02 +0100

    forth commith

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import Login from './components/Login';
 function App() {
   return (
     <div className="App">
-      <h1>My React App</h1>
       <Login />
     </div>
   );
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,12 +1,27 @@
 import React from 'react'
-import { Grid, Paper } from "@material-ui/core"
+import { Avatar, Grid, Paper } from "@material-ui/core"
+import LockOpenOutlinedIcon from '@material-ui/icons/LockOpenOutlined';
+
 
 function Login() {
+    const paperStyle = { padding: 20, height: '70vh', width: 380, margin: "20px auto" }
+    const AvatarStyle ={ backgroundColor: "green"}
     return (
+
+
         <Grid>
 
-            <Paper elevation={10}>
-                Signin
+            <Paper elevation={10} style={paperStyle}>
+
+                <Grid align="center">
+                    <Avatar style={AvatarStyle}>
+                        <LockOpenOutlinedIcon />
+                    </Avatar>
+                    <h2>Sign In</h2>
+
+                </Grid>
+
+
 
             </Paper>
 
